03200649e039a392bad6cacbd427884812d1e9fc
SanjanaKute <sanjanakute123@gmai.com>
2022-01-09 19:56:03 +0530

    Angula Assignment

---
+++ assignment-one/src/app/user/user.component.spec.ts
@@ -0,0 +1,22 @@
+
+import { UserComponent } from './user.component';
+
+describe('UserComponent', () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+
+});
